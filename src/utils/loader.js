e9efa2040b2e4ee0d2e4c0c0a7dd6f1ce1e8e237
Morena <morena.barboni@studenti.unicam.it>
2022-03-30 10:28:40 +0200

    Added support for safe-contracts dependencies

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 const glob = require("glob");
 const fileSys = require("./fileSys");
 const sol_parser = require("@solidity-parser/parser");
-const acorn = require("acorn");
+const ts = require('typescript');
 const config = require("../config");
+const { factory } = require("typescript");
 
 function loadTests() {
   fileSys.copyTestsToBaseline();
@@ -16,61 +17,61 @@ function loadTests() {
 
   const paths = glob.sync(fileSys.loadTestsDirGlob);
 
-  if (paths.length === 0) {
+  if (paths.length == 0) {
     console.log("Tests directory is empty!");
     process.exit(0);
   }
 
-  var tests = [];
+  var tests = new Array();
   paths.forEach((test) => {
     const content = fs.readFileSync(test);
 
-    var artifacts = []; //used_contracts
-    var requires = []; //used_tests
+    var artifacts = new Array(); //used_contracts
+    var requires = new Array(); //used_tests
 
-    if (test.endsWith(".js")||test.endsWith(".ts")) {
+    if (test.endsWith(".js")) {
       var ast = acorn.parse(content.toString(), {
-        ecmaVersion: 2020
+        ecmaVersion: 2020,
       });
       ast.body.forEach((node) => {
-        if (node.type === "VariableDeclaration") {
+        if (node.type == "VariableDeclaration") {
           if (
             node.declarations.length > 0 &&
-            (node.declarations[0]!==null && node.declarations[0]!== undefined) &&
-            (node.declarations[0].init!==null && node.declarations[0].init !== undefined) &&
-            node.declarations[0].init.arguments !== undefined &&
+            node.declarations[0] != undefined &&
+            node.declarations[0].init != undefined &&
+            node.declarations[0].init.arguments != undefined &&
             node.declarations[0].init.arguments.length > 0 &&
-            node.declarations[0].init.arguments[0].value !== undefined &&
-            node.declarations[0].init.callee !== undefined &&
-            node.declarations[0].init.callee.object !== undefined &&
-            node.declarations[0].init.callee.property !== undefined &&
-            node.declarations[0].init.callee.object.name === "artifacts" &&
-            node.declarations[0].init.callee.property.name === "require"
+            node.declarations[0].init.arguments[0].value != undefined &&
+            node.declarations[0].init.callee != undefined &&
+            node.declarations[0].init.callee.object != undefined &&
+            node.declarations[0].init.callee.property != undefined &&
+            node.declarations[0].init.callee.object.name == "artifacts" &&
+            node.declarations[0].init.callee.property.name == "require"
           ) {
             artifacts.push(node.declarations[0].init.arguments[0].value);
 
           } else if (
             node.declarations.length > 0 &&
-            (node.declarations[0]!==null && node.declarations[0]!== undefined) &&
-            (node.declarations[0].init!==null && node.declarations[0].init !== undefined) &&
-            node.declarations[0].init.arguments !== undefined &&
+            node.declarations[0] != undefined &&
+            node.declarations[0].init != undefined &&
+            node.declarations[0].init.arguments != undefined &&
             node.declarations[0].init.arguments.length > 0 &&
-            node.declarations[0].init.arguments[0].value !== undefined &&
-            node.declarations[0].init.callee !== undefined &&
-            node.declarations[0].init.callee.name !== undefined &&
-            node.declarations[0].init.callee.name === "require"
+            node.declarations[0].init.arguments[0].value != undefined &&
+            node.declarations[0].init.callee != undefined &&
+            node.declarations[0].init.callee.name != undefined &&
+            node.declarations[0].init.callee.name == "require"
           ) {
             requires.push(node.declarations[0].init.arguments[0].value);
           }
-        } else if (node.type === "ExpressionStatement") {
+        } else if (node.type == "ExpressionStatement") {
           if (
-            node.expression !== undefined &&
-            node.expression.callee !== undefined &&
-            node.expression.callee.name !== undefined &&
-            node.expression.callee.name === "require" &&
-            node.expression.arguments !== undefined &&
+            node.expression != undefined &&
+            node.expression.callee != undefined &&
+            node.expression.callee.name != undefined &&
+            node.expression.callee.name == "require" &&
+            node.expression.arguments != undefined &&
             node.expression.arguments.length > 0 &&
-            node.expression.arguments[0].value !== undefined
+            node.expression.arguments[0].value != undefined
           )
             requires.push(node.expression.arguments[0].value);
         }
@@ -80,22 +81,91 @@ function loadTests() {
       lines.forEach((l) => {
         if (l.includes(".sol")) artifacts.push(l.split("'", 2)[1]);
       });
-    } else if (test.endsWith(".sol")) {
+    } else if (test.endsWith(".ts")) {
+      //this is an ad-hoc solution for the Safe-Contracts project
+      const node = ts.createSourceFile(
+        test,   // fileName
+        fs.readFileSync(test, 'utf8'), // sourceText
+        ts.ScriptTarget.Latest // langugeVersion
+      );
+
+      var importDecl;
+
+      node.forEachChild(child => {
+        if (ts.SyntaxKind[child.kind] === 'ImportDeclaration') {
+          importDecl = child;
+        }
+
+       if (importDecl && importDecl.moduleSpecifier && importDecl.moduleSpecifier.text && importDecl.moduleSpecifier.text.includes('/utils/setup')) {
+        
+          if (importDecl.importClause && importDecl.importClause.namedBindings && importDecl.importClause.namedBindings.elements) {
+         
+            let importedElements = importDecl.importClause.namedBindings.elements.map(
+              el => el.name.escapedText
+            )
+            console.log(importedElements)
+
+            if (importedElements.includes("getFactory")) {
+              artifacts.push("GnosisSafeProxyFactory.sol");
+            }
+            if (importedElements.includes("getMock")) {
+              artifacts.push("MockContract.sol");
+            }
+            if (importedElements.includes("getSimulateTxAccessor")) {
+              artifacts.push("SimulateTxAccessor.sol");
+            }
+            if (importedElements.includes("getMultiSend")) {
+              artifacts.push("MultiSend.sol");
+            }
+            if (importedElements.includes("getMultiSendCallOnly")) {
+              artifacts.push("MultiSendCallOnly.sol");
+            }
+            if (importedElements.includes("defaultCallbackHandlerDeployment") || importedElements.includes("defaultCallbackHandlerContract")) {
+              artifacts.push("DefaultCallbackHandler.sol");
+            }
+            if (importedElements.includes("compatFallbackHandlerDeployment") ||
+              importedElements.includes("compatFallbackHandlerContract") ||
+              importedElements.includes("getDefaultCallbackHandler") ||
+              importedElements.includes("getCompatFallbackHandler")
+            ) {
+              artifacts.push("CompatibilityFallbackHandler.sol");
+            }
+            if (importedElements.includes("getSafeSingleton")) {
+              artifacts.push("GnosisSafe.sol");
+            }
+            if (importedElements.includes("getCreateCall")) {
+              artifacts.push("CreateCall.sol");
+            }
+            if (importedElements.includes("getSafeTemplate") || importedElements.includes("getSafeWithOwners")) {
+              artifacts.push("GnosisSafe.sol");
+              artifacts.push("GnosisSafeProxyFactory.sol");
+            }
+
+            console.log("Which are contracts: " +artifacts)
+
+          }
+        }
+      }
+      );
+    }
+
+
+    else if (test.endsWith(".sol")) {
       const content = fs.readFileSync(test);
 
       var ast = sol_parser.parse(content.toString());
       sol_parser.visit(ast, {
-        ImportDirective: function(node) {
+        ImportDirective: function (node) {
 
           //used contract is a test
-          if (node.path.includes(path.basename(config.testDir))) {
-            requires.push(path.basename(node.path).split(".sol")[0]);
+          if (node.path.includes(path.basename(config.testsDir))) {
+            requires.push(path.basename(node.path).split('.sol')[0]);
           }
           //used contract is a contract under test
           else if (node.path.includes(path.basename(config.contractsDir))) {
-            artifacts.push(path.basename(node.path).split(".sol")[0]);
+            artifacts.push(path.basename(node.path).split('.sol')[0]);
           }
-        }
+        },
       });
     }
 
@@ -104,12 +174,13 @@ function loadTests() {
       name: path.parse(test).base,
       content: content,
       used_tests: requires,
-      used_contracts: artifacts
+      used_contracts: artifacts,
     });
   });
 
   return tests;
 }
+
 
 function loadContracts() {
   fileSys.copyContractsToBaseline();
@@ -121,28 +192,28 @@ function loadContracts() {
 
   const paths = glob.sync(fileSys.loadContractsDirGlob);
 
-  if (paths.length === 0) {
+  if (paths.length == 0) {
     console.log("Contracts directory is empty!");
     process.exit(0);
   }
 
-  var contracts = [];
+  var contracts = new Array();
   paths.forEach((contract) => {
     const content = fs.readFileSync(contract);
 
-    var imports = []; //used_contracts
+    var imports = new Array(); //used_contracts
     var ast = sol_parser.parse(content.toString());
     sol_parser.visit(ast, {
-      ImportDirective: function(node) {
+      ImportDirective: function (node) {
         imports.push(node.path);
-      }
+      },
     });
 
     contracts.push({
       path: contract,
       name: path.parse(contract).base,
       content: content,
-      used_contracts: imports
+      used_contracts: imports,
     });
   });
   return contracts;
@@ -156,10 +227,54 @@ function loadMutationOperators() {
   return ops;
 }
 
+function loadMatrixJsonFromFile(matrix) {
+  var lines = matrix.toString().split(/(?:\r\n|\r|\n)/g);
+
+  var mutantLines = [];
+  lines.forEach((line) => {
+    if (line.length > 0) {
+      const contract_mutant_killers_saviors = line.split(":", 4);
+      const contract = contract_mutant_killers_saviors[0];
+      const mutant = contract_mutant_killers_saviors[1];
+      const killers = contract_mutant_killers_saviors[2];
+      const saviors = contract_mutant_killers_saviors[3];
+      var killers_array = killers.split(",");
+      if (killers_array[0] == "") killers_array = [];
+      var saviors_array = saviors.split(",");
+      if (saviors_array[0] == "") saviors_array = [];
+      const mutantLine = {
+        contract: contract,
+        mutant: mutant,
+        killers: killers_array,
+        saviors: saviors_array,
+      };
+
+      mutantLines.push(mutantLine);
+    }
+  });
+
+  return mutantLines;
+}
+
+function loadPreviousMatrixJson() {
+  return loadMatrixJsonFromFile(fileSys.loadPreviousMatrixFile());
+}
+
+function loadCurrentMatrixJson() {
+  return loadMatrixJsonFromFile(fileSys.loadCurrentMatrixFile());
+}
+
+function loadFinalMatrix() {
+  return loadMatrixJsonFromFile(
+    fs.readFileSync(require("../config").finalMatrixPath)
+  );
+}
 
 module.exports = {
   loadTests: loadTests,
   loadContracts: loadContracts,
-  loadMutationOperators: loadMutationOperators
-
+  loadMutationOperators: loadMutationOperators,
+  loadPreviousMatrixJson: loadPreviousMatrixJson,
+  loadCurrentMatrixJson: loadCurrentMatrixJson,
+  loadFinalMatrix: loadFinalMatrix,
 };
